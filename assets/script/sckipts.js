117062c3d16cdd6620af14d0ec7c1909907ec0fa
Olya <eremenko_olya@mail.ru>
2019-04-19 19:05:49 +0300

    data storage in Local Storage added

--- assets/script/sckipts.js
+++ assets/script/sckipts.js
@@ -1,3 +1,4 @@
+
 function showMenu() {
     let menu = document.getElementsByClassName('menu')[0];
     let content = document.getElementsByClassName('content')[0];
@@ -5,4 +6,19 @@ function showMenu() {
     menu.classList.toggle('menu_active');
     content.classList.toggle('content_active');
     btn.classList.toggle('menuBtn_active');
+}
+function save() {
+    let task = {
+        title: document.getElementById('title').value,
+        type: document.getElementById('type').value,
+        priority: document.getElementById('priority').value,
+        estimate: document.getElementById('estimate').value,
+        label: document.getElementById('labels').value,
+        reporter: document.getElementById('reporter').value,
+        description: document.getElementById('description').value,
+        id: Date.now()
+    };
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
